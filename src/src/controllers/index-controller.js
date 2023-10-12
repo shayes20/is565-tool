7d39d156a450fd680fe09d1ca7c0945f6cd93ed0
justinbrown11 <justin.j.brown@outlook.com>
2023-10-11 18:33:24 -0600

    more setup

--- src/src/controllers/index-controller.js
+++ src/src/controllers/index-controller.js
@@ -2,7 +2,10 @@ import httpStatus from 'http-status';
 import catchAsync from '../utils/catch-async.js';
 
 const indexController = catchAsync(async (req, res) => {
-    res.status(httpStatus[200]).send('ok');
+    res.status(httpStatus.OK).send('ok');
+    // if (some error condition) {
+    //     throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+    // }
 });
 
 export default indexController;
--- src/src/utils/valid-password.js
+++
@@ -1,4 +0,0 @@
-const validPassword =
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/;
-
-export default validPassword;
